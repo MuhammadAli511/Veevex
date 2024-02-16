db838fd849cc3d119fe1edd10c9303322200b518
Muhammad Ali <mohammadali417@outlook.com>
2024-02-16 18:57:41 +0500

    Changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 const connectDB = async () => {
-    let dbURI = process.env.MONGODB_URI;
+    let dbURI = process.env.MONGO_URI;
     try {
         const conn = await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log(`MongoDB Connected at ${conn.connection.host}`)
